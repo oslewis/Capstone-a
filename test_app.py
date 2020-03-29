868a22bbb48049db6224e85a3ac28f4df72d30c8
Omer S Lewis <contact@oslewiscom>
2020-03-29 05:12:13 +0100

    updated tokens

--- test_app.py
+++ test_app.py
@@ -1,8 +1,8 @@
 
-import os 
+import os
 import unittest
 import json
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 import app
 import models as models
 from app import create_app
@@ -38,12 +38,21 @@ class Capstone_me_TestCase(unittest.TestCase):
             "total_cashflow": 1000512,
         }
 
-        self.new_user = {
-            "id": 1,
-            "name": "John Doe",
-            "email": "john@example.com"
+
+        self.auth_specialist = {
+            'Authorization':
+            'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJUQXlRa1F5TVRKR1JETTBSa0ZGT0RnNFF6QTFRVVpDTVVJMVFqUkVNMFV6TURFNE9UUTNSZyJ9.eyJpc3MiOiJodHRwczovL2Zyb3N0eS1maXJlZmx5LTQ3NDMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVlN2UyNTMwMTkwMWY1MGNjODgzZGRlNyIsImF1ZCI6ImFwaSIsImlhdCI6MTU4NTQ1NDA3NiwiZXhwIjoxNTg1NDYxMjc2LCJhenAiOiJYNnc0bExBcEtYblBWbXl0bzNIbTRVT0UzUkZqMmk4dyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsicG9zdDpjYXNoZmxvdyIsInBvc3Q6ZGVidCIsInJlYWQ6ZGVidCIsInJlYWQ6aW5jb21lIl19.v22noxl1wORjtOo_dhOWOKRL7PvmLR_g09pFVihphVn2NioFim_drKrBY2F5jH0PNHlgfP0LTx4HLgqOCdSp3UvCYWq_lmFi7FdcmGzr0qf-G5GoPqJaPeSR1jFMYr9qxllt0z2UNFG80FjemEJDll87CUPtczLbZxG6bg9QW-1VqCYehusathYudNZIK556k--fEBCuiaQRaTg2JVAJOtRoB0_gfhrbOg0cc9PWdMjjM82OqGJVRDBSLfJWt68y3L--rI7qkMuNn5jRKt2yRS0vC7DBS-OTn7mPp40dD40Ez2lMRnfQAsOZeQN7TdL24FY-PhZGSlTxDhUlzLhDWA'
+            }
+
+        self.auth_operations_manager = {
+            'Authorization':
+            'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJUQXlRa1F5TVRKR1JETTBSa0ZGT0RnNFF6QTFRVVpDTVVJMVFqUkVNMFV6TURFNE9UUTNSZyJ9.eyJpc3MiOiJodHRwczovL2Zyb3N0eS1maXJlZmx5LTQ3NDMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVlN2UyNDRlNTkzOTAxMGNjOWMzNjI0YyIsImF1ZCI6ImFwaSIsImlhdCI6MTU4NTQ1MzkwMCwiZXhwIjoxNTg1NDYxMTAwLCJhenAiOiJYNnc0bExBcEtYblBWbXl0bzNIbTRVT0UzUkZqMmk4dyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmRlYnQiLCJkZWxldGU6aW5jb21lIiwicGF0Y2g6ZGVidCIsInBhdGNoOmluY29tZSIsInBvc3Q6Y2FzaGZsb3ciLCJwb3N0OmRlYnQiLCJyZWFkOmJhbGFuY2UiLCJyZWFkOmRlYnQiLCJyZWFkOmluY29tZSJdfQ.3is4tegh_AzbdxrxeYzJEmiQrrNOBy4cGSMQsN_pjRA5_PwDWf5K2mTTHvIkY-BPNGf5RtFwvOaKp3i7Io4jul4NxLfitRuY4trlKJMhrzb75rKkMpoygpFIXfbbYgzXynkfi5JYfzZGblL4Htvf9xrqM3qrasSlzu8y72uql6Sk8C0rRn16JVIvqKJS_54mC3uhQVx5JIIVmO8pZQPJanTMTXABPEHfsu8Z24wM44dcJGG7kTAssjZ1JtCLN3m3HiuV6HdN36h07F427Hb7ZOX18BpygdeEHh0dadLolamAKTteXpfPMkJrlTB6NWbVoEApFxw17iwkbE2eOP4RCA'
         }
 
+        self.auth_admin = {
+            'Authorization':
+            'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlJUQXlRa1F5TVRKR1JETTBSa0ZGT0RnNFF6QTFRVVpDTVVJMVFqUkVNMFV6TURFNE9UUTNSZyJ9.eyJpc3MiOiJodHRwczovL2Zyb3N0eS1maXJlZmx5LTQ3NDMuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDVlN2UyNGViYzBhYjM0MGNkNjY3NjQ4MCIsImF1ZCI6ImFwaSIsImlhdCI6MTU4NTQ1NDIxNCwiZXhwIjoxNTg1NDYxNDE0LCJhenAiOiJYNnc0bExBcEtYblBWbXl0bzNIbTRVT0UzUkZqMmk4dyIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmRlYnQiLCJkZWxldGU6aW5jb21lIiwicGF0Y2g6YmFsYW5jZSIsInBhdGNoOmRlYnQiLCJwYXRjaDppbmNvbWUiLCJwb3N0OmNhc2hmbG93IiwicG9zdDpkZWJ0IiwicmVhZDpiYWxhbmNlIiwicmVhZDpkZWJ0IiwicmVhZDppbmNvbWUiXX0.xvnBI0GcAcAMnRocgGi3NBP4_wpL0f4c-zSSNVS2BxIstNuuoKXF-smn5DtrAgagR73bEp8cpsH6RSDGHXAEV-wtmn9L7CD64_au-7oy8V1D1jHHAlQIsSKxqpRoXJfmS-2kS8FKVzMfQ60aRubcDx4TfOOblKziDiaM1WD38Jt4IV4g9Qq5R5F6By54epjOtuLwTtXkrRl49qe0TG8c1Mv08gb2DxbemSCkX3QF0fVe5ryVJlepxsoMQhvzSIS0dRfc4yEoOuxFzA4Buh83XaWZIg7hfUT6Qm6lRh3e8DGk_TR67h_AEys5Yxh0keW3E0wpbi0jghZ1r7klkNvTWg'
+            }
 
         # binds app to current context
         with self.app.app_context():
@@ -54,7 +63,7 @@ class Capstone_me_TestCase(unittest.TestCase):
 
     def tearDown(self):
         pass
-    
+
     def test_get_paginated_cashflow_items(self):
         res = self.client().get('/cashflow')
         data = json.loads(res.data)
@@ -76,7 +85,7 @@ class Capstone_me_TestCase(unittest.TestCase):
         self.assertTrue(data['total_cashflow'])
         self.assertEqual(cashflow, None)
 
-    
+
     def test_delete_debt_item(self):
         res = self.client().delete('/debt/1')
         data = json.loads(res.data)
@@ -90,18 +99,6 @@ class Capstone_me_TestCase(unittest.TestCase):
         self.assertEqual(debt, None)
 
 
-    def test_delete_user(self):
-        res = self.client().delete('/user/1')
-        data = json.loads(res.data)
-
-        user = User.query.filter(User.id == 1).one_or_none()
-
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], True)
-        self.assertEqual(data['delete'], 1)
-        self.assertEqual(user, None)
-
-
     def test_404_if_cashflow_item_does_not_exist(self):
         res = self.client().delete('/cashflow/10000')
         data = json.loads(res.data)
@@ -113,14 +110,6 @@ class Capstone_me_TestCase(unittest.TestCase):
 
     def test_404_if_debt_item_does_not_exist(self):
         res = self.client().delete('/debt/10000')
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 422)
-        self.assertEqual(data['success'], False)
-        self.assertEqual(data['message'], 'unprocessable')
-
-    def test_404_if_user_does_not_exist(self):
-        res = self.client().delete('/user/10000')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 422)
@@ -153,27 +142,17 @@ class Capstone_me_TestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['total_debt'])
 
-    def _test_create_new_user(self):
-        res = self.client().post('/user', json=self.debt)
-        data = json.loads(res.data)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], True)
 
     def test_422_cashflow_item_creation_fails(self):
         res = self.client().post('/cashflow', json=self.new_cashflow)
         data = json.loads(res.data)
         pass
-        
+
     def test_422_debt_item_creation_fails(self):
         res = self.client().post('/debt', json=self.new_debt)
         data = json.loads(res.data)
         pass
 
-    def test_422_user_creation_fails(self):
-        res = self.client().post('/user', json=self.new_user)
-        data = json.loads(res.data)
-        pass
 
     def _test_patch_cashflow_item(self):
         res = self.client().patch('/cashflow', json=self.cashflow)
@@ -188,14 +167,8 @@ class Capstone_me_TestCase(unittest.TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-    
-    def _test_patch_user_(self):
-        res = self.client().patch('/user', json=self.user)
-        data = json.loads(res.data)
 
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data['success'], True)
-    
+
     def _test_patch_balancesheet_(self):
         res = self.client().patch('/balancesheets', json=self.balancesheet)
         data = json.loads(res.data)
@@ -203,10 +176,6 @@ class Capstone_me_TestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
 
-    def test_422_user_patch_fails(self):
-        res = self.client().patch('/user', json=self.update_user)
-        data = json.loads(res.data)
-        pass
 
     def test_422_cashflow_item_patch_fails(self):
         res = self.client().patch('/cashflow', json=self.update_cashflow)
